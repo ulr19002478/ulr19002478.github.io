82bd0245076a11f59a77fa0e6ecbabf61a3ec88c
Oli Ulrico <ulr19002478@btc.ac.uk>
2024-12-12 15:12:01 +0000

    test

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const mainText = document.getElementById('main-text');
 const subText = document.getElementById('sub-text');
 const body = document.body;
 
-function animateText() {
-    const textElements = document.querySelectorAll('.magical-text span');
-    textElements.forEach((letter) => {
-        const randomDelay = Math.random() * 2;
-        letter.style.animationDelay = `${randomDelay}s`;
-    });
-}
+// Add a gradient that follows the mouse
+document.body.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    body.style.background = `
+        radial-gradient(
+            circle at ${mouseX}px ${mouseY}px, 
+            rgba(255, 183, 77, 0.8), 
+            rgba(228, 149, 76, 0.8) 50%, 
+            rgba(100, 50, 50, 0.5) 100%
+        )
+    `;
+});
 
 function wrapTextInSpans() {
     const magicalText = document.querySelectorAll('#main-text, #sub-text');
@@ -23,7 +30,6 @@ function wrapTextInSpans() {
             textElement.appendChild(span);
         });
     });
-    animateText();
 }
 
 function updateTextAndImage() {
@@ -32,27 +38,17 @@ function updateTextAndImage() {
         toggleImg.alt = 'the cat is awesome!';
         mainText.textContent = 'the cat';
         subText.textContent = 'what the cat doin?';
-        body.style.backgroundColor = '#6a9fb5';
         document.title = 'what the cat doin';
     } else {
         toggleImg.src = 'thedog.png';
         toggleImg.alt = 'the dog is awesome!';
         mainText.textContent = 'the dog';
         subText.textContent = 'what the dog doin?';
-        body.style.backgroundColor = '#e4954c';
         document.title = 'what the dog doin';
     }
 
     wrapTextInSpans();
 }
-
-// Create a mouse-following gradient effect
-document.body.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX / window.innerWidth * 100;
-    const mouseY = e.clientY / window.innerHeight * 100;
-
-    body.style.background = `radial-gradient(circle at ${mouseX}% ${mouseY}%, #f5a623, #e4954c)`;
-});
 
 window.onload = () => {
     wrapTextInSpans();
